2fd84dd45a5271cf52ff81b7e2c6939a0fcded21
Matthew Foemmel <git@foemmel.com>
2010-07-13 20:29:16 -0500

    repos tests now clear out the test repos before running

--- go/repos/repos_test.go
+++ go/repos/repos_test.go
@@ -1,11 +1,14 @@
 package repos
 
+import "os"
 import "testing"
 
 import . "fig/model"
 
 func TestListPackages(t *testing.T) {
-	r := &fileRepository{"test"}
+	r := resetRepos()
+	r.AddPackage(NewPackage("bar","4.5.6",".",[]*Config{}))
+	r.AddPackage(NewPackage("foo","1.2.3",".",[]*Config{}))
 	expected := []Descriptor{
 		NewDescriptor("bar","4.5.6",""),
 		NewDescriptor("foo","1.2.3",""),
@@ -26,7 +29,7 @@ func TestListPackages(t *testing.T) {
 }
 
 func TestAddPackage(t *testing.T) {
-	r := &fileRepository{"test"}
+	r := resetRepos()
 	pkg := NewPackage("baz","7.8.9",".",[]*Config{
 		NewConfig("default"),
 	})
@@ -34,4 +37,11 @@ func TestAddPackage(t *testing.T) {
 	if ok, msg := ComparePackage(pkg, r.LoadPackage("baz","7.8.9")); !ok {
 		t.Error(msg)
 	}
+}
+
+func resetRepos() *fileRepository {
+	path := "testrepos"
+	os.RemoveAll(path)
+	os.Mkdir(path,0777)
+	return &fileRepository{path}
 }
