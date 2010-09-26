dab27211a226efe5e2a75b27c576921c9c854803
Matthew Foemmel <git@foemmel.com>
2010-09-25 23:56:34 -0500

    initial work on NativeFileSystem

---
+++ go/fs_native.go
@@ -0,0 +1,36 @@
+package fig
+
+import "io"
+import "os"
+
+type nativeFileSystem struct {
+}
+
+func NewNativeFileSystem() FileSystem {
+	return &nativeFileSystem{}
+}
+
+func (fs *nativeFileSystem) Exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if pathErr, ok := err.(*os.PathError); ok {
+		if pathErr.Error == os.ENOENT {
+			return false
+		}
+	}
+	panic(err)
+}
+
+func (fs *nativeFileSystem) Size(path string) (int64, os.Error) {
+	panic("not implemented")
+}
+
+func (fs *nativeFileSystem) OpenReader(path string) (io.ReadCloser, os.Error) {
+	panic("not implemented")
+}
+
+func (fs *nativeFileSystem) OpenWriter(path string) (io.WriteCloser, os.Error) {
+	return os.Open(path, os.O_CREAT|os.O_WRONLY, 0666)
+}
