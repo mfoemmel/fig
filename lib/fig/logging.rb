895e586a521c2c5fc806321bd3404c3ffbd6e65b
Elliot Shank <eshank@drw.com>
2011-10-27 17:05:43 -0500

    Handle logging while running spec tests.

--- lib/fig/logging.rb
+++ lib/fig/logging.rb
@@ -3,12 +3,19 @@ require 'log4r'
 module Fig; end
 
 module Fig::Logging
-  def self.initialize_logging
+  def self.initialize_logging(
+    config_file = nil,
+    suppress_default_configuration = false
+  )
     logger = Log4r::Logger.new('fig')
 
-    outputter = Log4r::Outputter.stdout
-    logger.add outputter
-    outputter.formatter = Log4r::PatternFormatter.new :pattern => '%M'
+    if config_file
+      raise NotImplementedError, %q<Haven't handled config files yet.>
+    elsif not suppress_default_configuration
+      outputter = Log4r::Outputter.stdout
+      logger.add outputter
+      outputter.formatter = Log4r::PatternFormatter.new :pattern => '%M'
+    end
 
     return
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'log4r'
+require 'stringio'
+
 require 'fig'
 require 'fig/logging'
 
-Fig::Logging.initialize_logging
+STRING_IO = StringIO.new
+Fig::Logging.initialize_logging(nil, true)
+Log4r::Logger['fig'].add( Log4r::IOOutputter.new('fig', STRING_IO) )
