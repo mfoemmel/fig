a5ddb93901de7d38157a73d3bf4f2152c7978785
Kent Cowgill <kent@c2group.net>
2011-11-02 14:23:03 -0500

    Adding whitelisting to ApplicationConfiguration for allowed repository URLs

--- lib/fig/applicationconfiguration.rb
+++ lib/fig/applicationconfiguration.rb
@@ -1,6 +1,22 @@
 
 module Fig
   class ApplicationConfiguration
+    def initialize(remote_repository_url)
+      @data = []
+      @remote_repository_url = remote_repository_url
+      clear_cached_data
+    end
+
+    def ensure_url_whitelist_initialized()
+      return if not @whitelist.nil?
+      whitelist = self['url_whitelist']
+      if whitelist.nil?
+        @whitelist = []
+      else
+        @whitelist = [@remote_repository_url, whitelist].flatten
+      end
+    end
+
     def [](key)
       @data.each do |dataset|
         if dataset.has_key?(key)
@@ -18,8 +34,19 @@ module Fig
       @data.unshift(dataset)
     end
 
-    def initialize()
-      @data = []
+    # after push_dataset or unshift_dataset, call clear_cached, and lazy initialize as far as the list of things to exclude
+
+    def clear_cached_data()
+      @whitelist = nil
+    end
+
+    def url_access_allowed?(url)
+      ensure_url_whitelist_initialized
+      return true if @whitelist.empty?
+      @whitelist.each do |allowed_url|
+        return true if url.match(/\A#{Regexp.quote(allowed_url)}\b/)
+      end
+      return false
     end
   end
 end
