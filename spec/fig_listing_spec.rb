2202629a9d3d60b7582b7a4faf5e49fb21876cb6
Elliot Shank <eshank@drw.com>
2012-01-05 11:58:29 -0600

    Add tests for --list-configs.

--- spec/fig_listing_spec.rb
+++ spec/fig_listing_spec.rb
@@ -13,12 +13,26 @@ def set_up_local_and_remote_repository
     config default
       set FOO=BAR
     end
+    config nondefault
+      set FOO=BAZ
+    end
   END
 
   fig('--publish remote-only/1.2.3', input)
   fig('--clean remote-only/1.2.3', input)
   fig('--publish both/1.2.3', input)
   fig('--publish-local local-only/1.2.3', input)
+
+  return
+end
+
+def test_list_configs(package_name)
+  set_up_local_and_remote_repository
+
+  (out, err, exitstatus) = fig("--list-configs #{package_name}/1.2.3")
+  exitstatus.should == 0
+  out.should == "default\nnondefault"
+  err.should == ''
 
   return
 end
@@ -61,6 +75,25 @@ describe 'Fig' do
       exitstatus.should == 0
       out.should == "both/1.2.3\nremote-only/1.2.3"
       err.should == ''
+    end
+  end
+
+  describe '--list-configs' do
+    it %q<prints all the configurations for local-only> do
+      test_list_configs('local-only')
+    end
+
+    it %q<prints all the configurations for both> do
+      test_list_configs('both')
+    end
+
+    it %q<prints all the configurations for remote-only> do
+      set_up_local_and_remote_repository
+
+      (out, err, exitstatus) =
+        fig("--list-configs remote-only}/1.2.3", nil, :no_raise_on_error)
+      exitstatus.should_not == 0
+      out.should =~ /Fig file not found for package/
     end
   end
 end
