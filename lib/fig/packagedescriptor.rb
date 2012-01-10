9210aad683afcdccb6759f1a61371abcc7de0717
Elliot Shank <eshank@drw.com>
2012-01-10 17:29:25 -0600

    Add PackageDescriptor.to_string().

--- lib/fig/packagedescriptor.rb
+++ lib/fig/packagedescriptor.rb
@@ -2,6 +2,8 @@ module Fig; end
 
 # Parsed representation of a package name:config/version.
 class Fig::PackageDescriptor
+  DEFAULT_CONFIG = 'default'
+
   attr_reader :name, :version, :config
 
   def initialize(raw_string)
@@ -9,5 +11,23 @@ class Fig::PackageDescriptor
     @name    = raw_string =~ %r< ^ ( [^:/]+ ) >x ? $1 : nil
     @config  = raw_string =~ %r< : ( [^:/]+ ) >x ? $1 : nil
     @version = raw_string =~ %r< / ( [^:/]+ ) >x ? $1 : nil
+  end
+
+  def to_string(use_default_config = false)
+    string = @name || ''
+
+    if @config
+      string += ':'
+      string += @config
+    elsif use_default_config
+      string += ':default'
+    end
+
+    if @version
+      string += '/'
+      string += @version
+    end
+
+    return string
   end
 end
