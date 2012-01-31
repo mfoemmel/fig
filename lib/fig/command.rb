a73d770cb3abc899c964697ffe66cd064c4b0044
Elliot Shank <eshank@drw.com>
2012-01-31 12:26:49 -0600

    Split out Command parts for package file loading.

--- lib/fig/command.rb
+++ lib/fig/command.rb
@@ -16,17 +16,21 @@ require 'fig/statement/configuration'
 require 'fig/statement/publish'
 require 'fig/userinputerror'
 
-# This is a breakout of part of this class simply to keep the file size down.
+# These are a breakout of parts of this class simply to keep the file size down.
+
 # You will need to look in this file for any stuff related to --list-* options.
 require 'fig/command/listing'
+
+# You will need to look in this file for any stuff related to loading the
+# primary Package object.
+require 'fig/command/packageload'
 
 module Fig; end
 
 # Main program
 class Fig::Command
   include Fig::Command::Listing
-
-  DEFAULT_FIG_FILE = 'package.fig'
+  include Fig::Command::PackageLoad
 
   def run_fig(argv)
     @options = Fig::Options.new(argv)
@@ -51,7 +55,7 @@ class Fig::Command
       return publish()
     end
 
-    get_package()
+    load_package_object()
 
     if @options.listing()
       handle_post_parse_list_options()
@@ -170,98 +174,10 @@ class Fig::Command
     end
   end
 
-  def read_in_package_config_file(config_file)
-    if File.exist?(config_file)
-      return File.read(config_file)
-    else
-      raise Fig::UserInputError.new(%Q<File not found: "#{config_file}".>)
-    end
-  end
-
   def remote_operation_necessary?()
     return @options.updating?                     ||
            @options.publish?                      ||
            @options.listing == :remote_packages
-  end
-
-  def load_package_config_file_contents()
-    package_config_file = @options.package_config_file()
-
-    if package_config_file == :none
-      return nil
-    elsif package_config_file == '-'
-      return $stdin.read
-    elsif package_config_file.nil?
-      if File.exist?(DEFAULT_FIG_FILE)
-        return File.read(DEFAULT_FIG_FILE)
-      end
-    else
-      return read_in_package_config_file(package_config_file)
-    end
-
-    return
-  end
-
-  def register_package_with_environment_if_not_listing()
-    return if @options.listing
-
-    register_package_with_environment()
-
-    return
-  end
-
-  def register_package_with_environment()
-    if @options.updating?
-      @package.retrieves.each do |var, path|
-        @environment.add_retrieve(var, path)
-      end
-    end
-
-    @environment.register_package(@package)
-    @environment.apply_config(
-      @package,
-      @options.config() || @descriptor && @descriptor.config() || 'default',
-      nil
-    )
-    return
-  end
-
-  def parse_package_config_file(config_raw_text)
-    if config_raw_text.nil?
-      @package = Fig::Package.new(nil, nil, '.', [])
-      return
-    end
-
-    @package =
-      Fig::Parser.new(@configuration).parse_package(
-        nil, nil, '.', config_raw_text
-      )
-
-    register_package_with_environment_if_not_listing()
-
-    return
-  end
-
-  def load_package_file()
-    config_raw_text = load_package_config_file_contents()
-
-    parse_package_config_file(config_raw_text)
-  end
-
-  def get_package()
-    if @descriptor.nil?
-      load_package_file()
-    else
-      # TODO: complain if config file was specified on the command-line.
-      @package =
-        @repository.get_package(
-          @descriptor.name, @descriptor.version, :disable_updating
-        )
-
-      register_package_with_environment_if_not_listing()
-    end
-
-    return
   end
 
   def publish()
