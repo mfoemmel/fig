7242862d86ce346c155776147867db31b5b311b7
Adam Komblevitz <komblevitz@gmail.com>
2012-04-11 13:58:39 -0500

    Adding custom RSpec formatter to show seed first.

---
+++ spec/support/formatters/seed_spitter.rb
@@ -0,0 +1,10 @@
+require 'rspec/core/formatters/documentation_formatter'
+
+class SeedSpitter < RSpec::Core::Formatters::DocumentationFormatter
+
+  def start(*)
+    super
+    output.puts "\nSeed: #{RSpec.configuration.seed}"
+  end
+
+end
