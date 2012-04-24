5555ac9b7f67da2dbde22aef1b0a1933d2b71593
Elliot Shank <eshank@drw.com>
2012-04-24 14:53:08 -0500

    Get unparse_statements() out of the global namespace.

--- lib/fig/statement/configuration.rb
+++ lib/fig/statement/configuration.rb
@@ -47,4 +47,13 @@ class Fig::Statement::Configuration < Fig::Statement
   def unparse(indent)
     unparse_statements(indent, "config #{@name}", @statements, 'end')
   end
+
+  private
+
+  def unparse_statements(indent, prefix, statements, suffix)
+    body =
+      @statements.map {|statement| statement.unparse(indent + '  ') }.join("\n")
+
+    return ["\n#{indent}#{prefix}", body, "#{indent}#{suffix}"].join("\n")
+  end
 end
