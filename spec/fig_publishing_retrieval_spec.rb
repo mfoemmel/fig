f0b80ab01b94a15dfe4132a70e9d4a3592b8f2c7
Elliot Shank <eshank@drw.com>
2012-04-30 14:00:58 -0500

    Give symbolic names for things in fig_publishing_retrieval_spec.

--- spec/fig_publishing_retrieval_spec.rb
+++ spec/fig_publishing_retrieval_spec.rb
@@ -4,55 +4,55 @@ require 'fig/operatingsystem'
 
 describe 'Fig' do
   describe 'publishing/retrieval' do
+    let(:lib_directory)      { "#{FIG_SPEC_BASE_DIRECTORY}/lib" }
+    let(:retrieve_directory) { "#{FIG_SPEC_BASE_DIRECTORY}/retrieve" }
+
     before(:each) do
       cleanup_test_environment
+      FileUtils.mkdir_p(lib_directory)
     end
 
     it 'retrieves resource' do
-      FileUtils.mkdir_p("#{FIG_SPEC_BASE_DIRECTORY}/lib")
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/a-library", 'w') { |f| f << 'some library' }
+      File.open("#{lib_directory}/a-library", 'w') { |f| f << 'some library' }
       input = <<-END
         resource lib/a-library
         config default
           append FOOPATH=@/lib/a-library
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
       input = <<-END
-        retrieve FOOPATH->lib2/[package]
+        retrieve FOOPATH->retrieve/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
         end
       END
       fig('--update-if-missing', input)
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/lib2/foo/a-library").should == 'some library'
+      File.read("#{retrieve_directory}/prerequisite/a-library").should == 'some library'
     end
 
     it 'retrieves resource and ignores the append statement in the updating config' do
-      FileUtils.mkdir_p("#{FIG_SPEC_BASE_DIRECTORY}/lib")
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/a-library", 'w') { |f| f << 'some library' }
+      File.open("#{lib_directory}/a-library", 'w') { |f| f << 'some library' }
       input = <<-END
         resource lib/a-library
         config default
           append FOOPATH=@/lib/a-library
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
       input = <<-END
-        retrieve FOOPATH->lib2/[package]
+        retrieve FOOPATH->retrieve/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
           append FOOPATH=@/does/not/exist
         end
       END
       fig('--update-if-missing', input)
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/lib2/foo/a-library").should == 'some library'
+      File.read("#{retrieve_directory}/prerequisite/a-library").should == 'some library'
     end
 
-
     it 'retrieves resource that is a directory' do
-      FileUtils.mkdir_p("#{FIG_SPEC_BASE_DIRECTORY}/lib")
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/a-library", 'w') { |f| f << 'some library' }
+      File.open("#{lib_directory}/a-library", 'w') { |f| f << 'some library' }
       # To copy the contents of a directory, instead of the directory itself,
       # use '/.' as a suffix to the directory name in 'append'.
       input = <<-END
@@ -61,15 +61,15 @@ describe 'Fig' do
           append FOOPATH=@/lib/.
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
       input = <<-END
-        retrieve FOOPATH->lib2/[package]
+        retrieve FOOPATH->retrieve/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
         end
       END
       fig('--update-if-missing', input)
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/lib2/foo/a-library").should == 'some library'
+      File.read("#{retrieve_directory}/prerequisite/a-library").should == 'some library'
     end
 
     it %q<preserves the path after '//' when copying files into your project directory while retrieving> do
@@ -84,18 +84,18 @@ describe 'Fig' do
           append INCLUDE=@//include/hello2.h
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
 
       input = <<-END
         retrieve INCLUDE->include2/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
         end
       END
       fig('--update', input)
 
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/foo/include/hello.h").should == 'a header file'
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/foo/include/hello2.h").should == 'another header file'
+      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/prerequisite/include/hello.h").should == 'a header file'
+      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/prerequisite/include/hello2.h").should == 'another header file'
     end
 
     it 'updates without there being a copy of the package in the FIG_HOME left there from publishing' do
@@ -110,26 +110,25 @@ describe 'Fig' do
           append INCLUDE=@/include/hello2.h
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
 
       FileUtils.rm_rf(FIG_SPEC_BASE_DIRECTORY + '/fighome')
 
       input = <<-END
         retrieve INCLUDE->include2/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
         end
       END
       fig('-u', input)
 
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/foo/hello.h").should == 'a header file'
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/foo/hello2.h").should == 'another header file'
+      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/prerequisite/hello.h").should == 'a header file'
+      File.read("#{FIG_SPEC_BASE_DIRECTORY}/include2/prerequisite/hello2.h").should == 'another header file'
     end
 
     it 'packages multiple resources' do
-      FileUtils.mkdir_p("#{FIG_SPEC_BASE_DIRECTORY}/lib")
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/a-library", 'w') { |f| f << 'some library' }
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/a-library2", 'w') { |f| f << 'some other library' }
+      File.open("#{lib_directory}/a-library", 'w') { |f| f << 'some library' }
+      File.open("#{lib_directory}/a-library2", 'w') { |f| f << 'some other library' }
       input = <<-END
         resource lib/a-library
         resource lib/a-library2
@@ -138,37 +137,36 @@ describe 'Fig' do
           append FOOPATH=@/lib/a-library2
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
       input = <<-END
-        retrieve FOOPATH->lib2/[package]
+        retrieve FOOPATH->retrieve/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
         end
       END
       fig('-m', input)
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/lib2/foo/a-library").should == 'some library'
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/lib2/foo/a-library2").should == 'some other library'
+      File.read("#{retrieve_directory}/prerequisite/a-library").should == 'some library'
+      File.read("#{retrieve_directory}/prerequisite/a-library2").should == 'some other library'
     end
 
     it 'packages multiple resources with wildcards' do
-      FileUtils.mkdir_p("#{FIG_SPEC_BASE_DIRECTORY}/lib")
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/foo.jar", 'w') { |f| f << 'some library' }
-      File.open("#{FIG_SPEC_BASE_DIRECTORY}/lib/bar.jar", 'w') { |f| f << 'some other library' }
+      File.open("#{lib_directory}/foo.jar", 'w') { |f| f << 'some library' }
+      File.open("#{lib_directory}/bar.jar", 'w') { |f| f << 'some other library' }
       input = <<-END
         resource **/*.jar
         config default
           append FOOPATH=@/lib/foo.jar
         end
       END
-      fig('--publish foo/1.2.3', input)
+      fig('--publish prerequisite/1.2.3', input)
       input = <<-END
-        retrieve FOOPATH->lib2/[package]
+        retrieve FOOPATH->retrieve/[package]
         config default
-          include foo/1.2.3
+          include prerequisite/1.2.3
         end
       END
       fig('--update-if-missing', input)
-      File.read("#{FIG_SPEC_BASE_DIRECTORY}/lib2/foo/foo.jar").should == 'some library'
+      File.read("#{retrieve_directory}/prerequisite/foo.jar").should == 'some library'
     end
 
     it 'warns on unused retrieval' do
