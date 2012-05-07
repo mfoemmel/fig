77ba7aaba0fed0992030a08fc65c9bbf6bfcf62c
Elliot Shank <eshank@drw.com>
2012-05-07 15:07:06 -0500

    Remove full usage message from "Nothing to do." message.

--- lib/fig/command.rb
+++ lib/fig/command.rb
@@ -111,8 +111,7 @@ class Fig::Command
         @options.command_extra_argv || []
       ) { |cmd| @operating_system.shell_exec cmd }
     elsif not @repository.updating?
-      $stderr.puts "Nothing to do.\n"
-      $stderr.puts Fig::Command::Options::USAGE
+      $stderr.puts "Nothing to do.\n\n"
       $stderr.puts %q<Run "fig --help" for a full list of commands.>
       return 1
     end
