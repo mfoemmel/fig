17df8f9e6da60c2d256ab009d90d72ecefbf5d0e
Elliot Shank <eshank@drw.com>
2012-05-08 11:12:04 -0500

    Add accessors to exceptions.

--- lib/fig/configfileerror.rb
+++ lib/fig/configfileerror.rb
@@ -4,6 +4,8 @@ module Fig
   # Could not determine some kind of information from a configuration file,
   # whether .figrc, log4r, package.fig, etc.
   class ConfigFileError < UserInputError
+    attr_accessor :file
+
     def initialize(message, file)
       super(message)
 
--- lib/fig/log4rconfigerror.rb
+++ lib/fig/log4rconfigerror.rb
@@ -3,6 +3,8 @@ require 'fig/userinputerror'
 module Fig
   # A problem with configuring Log4r.
   class Log4rConfigError < UserInputError
+    attr_accessor :config_file, :original_exception
+
     def initialize(config_file, original_exception)
       super(
         %Q<Problem with #{config_file}: #{original_exception.message}>
