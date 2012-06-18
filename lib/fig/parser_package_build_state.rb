fb0043502a191417ba987e428361036100d8d0f6
Elliot Shank <eshank@drw.com>
2012-06-18 14:34:56 -0500

    Move all Statement construction out of the grammar.

--- lib/fig/parser_package_build_state.rb
+++ lib/fig/parser_package_build_state.rb
@@ -1,5 +1,15 @@
+require 'fig/package'
 require 'fig/package_parse_error'
 require 'fig/statement'
+require 'fig/statement/archive'
+require 'fig/statement/command'
+require 'fig/statement/configuration'
+require 'fig/statement/include'
+require 'fig/statement/override'
+require 'fig/statement/path'
+require 'fig/statement/resource'
+require 'fig/statement/retrieve'
+require 'fig/statement/set'
 
 module Fig; end
 
@@ -33,6 +43,77 @@ class Fig::ParserPackageBuildState
     )
   end
 
+  def new_package_statement(directory, statements)
+    return Fig::Package.new(
+      descriptor.name,
+      descriptor.version,
+      directory,
+      statements.elements.map do
+        |statement|
+        statement.to_package_statement(self)
+      end
+    )
+  end
+
+  def new_asset_statement(statement_class, keyword_node, url_node)
+    return statement_class.new(
+      node_location(keyword_node), source_description, url_node.value.text_value
+    )
+  end
+
+  def new_retrieve_statement(keyword_node, variable_name_node, path_node)
+    return Fig::Statement::Retrieve.new(
+      node_location(keyword_node),
+      source_description,
+      variable_name_node.text_value,
+      path_node.text_value
+    )
+  end
+
+  def new_configuration_statement(keyword_node, name_node, statements)
+    return Fig::Statement::Configuration.new(
+      node_location(keyword_node),
+      source_description,
+      name_node.text_value,
+      statements.elements.map do
+        |statement|
+        statement.to_config_statement(self)
+      end
+    )
+  end
+
+  def new_include_statement(keyword_node, descriptor_node)
+    include_descriptor =
+      Fig::Statement::Include.parse_descriptor(
+        descriptor_node.text_value.strip,
+        :source_description => node_location_description(descriptor_node),
+        :validation_context => ' for an include statement'
+      )
+
+    return Fig::Statement::Include.new(
+      node_location(keyword_node),
+      source_description,
+      include_descriptor,
+      descriptor
+    )
+  end
+
+  def new_override_statement(keyword_node, descriptor_node)
+    override_descriptor =
+      Fig::Statement::Override.parse_descriptor(
+        descriptor_node.text_value.strip,
+        :source_description => node_location_description(descriptor_node),
+        :validation_context => ' for an override statement'
+      )
+
+    return Fig::Statement::Override.new(
+      node_location(keyword_node),
+      source_description,
+      override_descriptor.name,
+      override_descriptor.version
+    )
+  end
+
   def new_environment_variable_statement(
     statement_class, keyword_node, value_node
   )
@@ -45,6 +126,14 @@ class Fig::ParserPackageBuildState
     }
     return statement_class.new(
       node_location(keyword_node), source_description, name, value
+    )
+  end
+
+  def new_command_statement(keyword_node, command_node)
+    return Fig::Statement::Command.new(
+      node_location(keyword_node),
+      source_description,
+      command_node.value.text_value
     )
   end
 
