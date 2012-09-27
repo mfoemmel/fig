c85382189aaa08f50812be30dcf49f7f52815712
Elliot Shank <eshank@drw.com>
2012-09-27 14:19:00 -0500

    Fix v1 grammar parsing of quoted environment variable statements.

--- spec/command/grammar_environment_variable_spec.rb
+++ spec/command/grammar_environment_variable_spec.rb
@@ -8,12 +8,43 @@ describe 'Fig' do
       set_up_test_environment
     end
 
+    ENVIRONMENT_VARIABLE_PUBLISHED_QUOTING_EXAMPLES = {
+      [%q<VARIABLE='foo\'bar'>, 1] => [
+        %q<VARIABLE=foo\'bar>,
+        %q<VARIABLE="foo'bar">,
+        %q<VARIABLE='foo\'bar'>
+      ],
+      [%q<VARIABLE='foo"bar'>,  1] => [
+        %q<VARIABLE=foo\"bar>,
+        %q<VARIABLE="foo\"bar">,
+        %q<VARIABLE='foo"bar'>
+      ],
+      [%q<VARIABLE='foo#bar'>,  1] => [
+        %q<VARIABLE="foo#bar">,
+        %q<VARIABLE='foo#bar'>
+      ],
+      [%q<VARIABLE=foo@bar>,    0] => [
+        %q<VARIABLE=foo@bar>,
+        %q<VARIABLE="foo@bar">,
+      ],
+      [%q<VARIABLE=foo\@bar>,   0] => [
+        %q<VARIABLE=foo\@bar>,
+        %q<VARIABLE="foo\@bar">,
+        %q<VARIABLE='foo@bar'>
+      ],
+      [%q<VARIABLE=foo\\\\bar>,   0] => [
+        %q<VARIABLE=foo\\\\bar>,
+        %q<VARIABLE="foo\\\\bar">,
+        %q<VARIABLE='foo\\\\bar'>
+      ]
+    }
+
     shared_examples_for 'environment variable option' do
       |assignment_type|
 
       it 'with simple value' do
         fig(
-          [%w< --publish foo/1.2.3>, "--#{assignment_type}", 'VARIABLE=VALUE'],
+          [%w<--publish foo/1.2.3>, "--#{assignment_type}", 'VARIABLE=VALUE'],
           :fork => false
         )
 
@@ -32,7 +63,7 @@ describe 'Fig' do
         it "with #{name} whitespace" do
           fig(
             [
-              %w< --publish foo/1.2.3>,
+              %w<--publish foo/1.2.3>,
               "--#{assignment_type}",
               "VARIABLE=#{quote}foo bar#{quote}"
             ],
@@ -45,37 +76,7 @@ describe 'Fig' do
         end
       end
 
-      {
-        [%q<VARIABLE='foo\'bar'>, 1] => [
-          %q<VARIABLE=foo\'bar>,
-          %q<VARIABLE="foo'bar">,
-          %q<VARIABLE='foo\'bar'>
-        ],
-        [%q<VARIABLE='foo"bar'>,  1] => [
-          %q<VARIABLE=foo\"bar>,
-          %q<VARIABLE="foo\"bar">,
-          %q<VARIABLE='foo"bar'>
-        ],
-        [%q<VARIABLE='foo#bar'>,  1] => [
-          %q<VARIABLE=foo#bar>,
-          %q<VARIABLE="foo#bar">,
-          %q<VARIABLE='foo#bar'>
-        ],
-        [%q<VARIABLE=foo@bar>,    0] => [
-          %q<VARIABLE=foo@bar>,
-          %q<VARIABLE="foo@bar">,
-        ],
-        [%q<VARIABLE=foo\@bar>,   0] => [
-          %q<VARIABLE=foo\@bar>,
-          %q<VARIABLE="foo\@bar">,
-          %q<VARIABLE='foo@bar'>
-        ],
-        [%q<VARIABLE=foo\\\\bar>,   0] => [
-          %q<VARIABLE=foo\\\\bar>,
-          %q<VARIABLE="foo\\\\bar">,
-          %q<VARIABLE='foo\\\\bar'>
-        ]
-      }.each do
+      ENVIRONMENT_VARIABLE_PUBLISHED_QUOTING_EXAMPLES.each do
         |expected, inputs|
 
         result, version = *expected
@@ -85,7 +86,7 @@ describe 'Fig' do
 
           it "with «#{value}»" do
             fig(
-              [ %w< --publish foo/1.2.3>, "--#{assignment_type}", value ],
+              [ %w<--publish foo/1.2.3>, "--#{assignment_type}", value ],
               :fork => false
             )
 
@@ -94,6 +95,21 @@ describe 'Fig' do
             out.should =~ / \b #{assignment_type} \s+ #{Regexp.quote result} /x
           end
         end
+      end
+
+      it "with «VARIABLE=foo#bar»" do
+        fig(
+          [
+            %w<--publish foo/1.2.3>,
+            "--#{assignment_type}",
+            %q<VARIABLE=foo#bar>
+          ],
+          :fork => false
+        )
+
+        out, * = check_published_grammar_version(1)
+
+        out.should =~ / \b #{assignment_type} \s+ VARIABLE='foo[#]bar' /x
       end
 
       {
@@ -105,7 +121,7 @@ describe 'Fig' do
         it "with #{name}, unescaped at sign, but forced to v1 grammar" do
           fig(
             [
-              %w< --publish foo/1.2.3>,
+              %w<--publish foo/1.2.3>,
               "--#{assignment_type}",
               'VARIABLE_WITH_WHITESPACE_TO_FORCE=v1 grammar',
               "--#{assignment_type}",
@@ -140,7 +156,7 @@ describe 'Fig' do
 
           out, * = check_published_grammar_version(0)
 
-          out.should =~ / \b set \s+ VARIABLE=#{Regexp.quote character} /x
+          out.should =~ / \b set \s+ VARIABLE=#{Regexp.quote character} [^'] /x
         end
       end
     end
@@ -159,6 +175,118 @@ describe 'Fig' do
             ],
             :fork => false
           )
+
+          out, * = check_published_grammar_version(1)
+
+          out.should =~ / \b append \s+ VARIABLE='#{Regexp.quote character}' /x
+        end
+      end
+    end
+
+    shared_examples_for 'environment variable statement' do
+      |assignment_type|
+
+      it 'with simple value' do
+        input = <<-"END_INPUT"
+          grammar v1
+          config default
+            #{assignment_type} VARIABLE=VALUE
+          end
+        END_INPUT
+
+        fig %w<--publish foo/1.2.3>, input, :fork => false
+
+        out, * = check_published_grammar_version(0)
+
+        out.should =~ / \b #{assignment_type} \s+ VARIABLE=VALUE \b /x
+      end
+
+      {
+        'double quoted'   => %q<">,
+        'single quoted'   => %q<'>
+      }.each do
+        |name, quote|
+
+        it "with #{name} whitespace" do
+          input = <<-"END_INPUT"
+            grammar v1
+            config default
+              #{assignment_type} VARIABLE=#{quote}foo bar#{quote}
+            end
+          END_INPUT
+
+          fig %w<--publish foo/1.2.3>, input, :fork => false
+
+          out, * = check_published_grammar_version(1)
+
+          out.should =~ / \b #{assignment_type} \s+ VARIABLE='foo[ ]bar' /x
+        end
+      end
+
+      ENVIRONMENT_VARIABLE_PUBLISHED_QUOTING_EXAMPLES.each do
+        |expected, inputs|
+
+        result, version = *expected
+
+        inputs.each do
+          |value|
+
+          it "with «#{value}»" do
+            input = <<-"END_INPUT"
+              grammar v1
+              config default
+                #{assignment_type} #{value}
+              end
+            END_INPUT
+
+            fig %w<--publish foo/1.2.3>, input, :fork => false
+
+            out, * = check_published_grammar_version(version)
+
+            out.should =~ / \b #{assignment_type} \s+ #{Regexp.quote result} /x
+          end
+        end
+      end
+    end
+
+    describe 'for set statement in v1 grammar' do
+      it_behaves_like 'environment variable statement', 'set'
+
+      ILLEGAL_CHARACTERS_IN_V0_PATH_STATEMENTS.each do
+        |character|
+
+        it "for «#{character}»" do
+          input = <<-"END_INPUT"
+            grammar v1
+            config default
+              set VARIABLE=#{character}
+            end
+          END_INPUT
+
+          fig %w<--publish foo/1.2.3>, input, :fork => false
+
+          out, * = check_published_grammar_version(0)
+
+          out.should =~ / \b set \s+ VARIABLE=#{Regexp.quote character} [^'] /x
+        end
+      end
+    end
+
+    describe 'for append statement in v1 grammar' do
+      it_behaves_like 'environment variable statement', 'append'
+
+      ILLEGAL_CHARACTERS_IN_V0_PATH_STATEMENTS.each do
+        |character|
+
+        it "for «#{character}»" do
+          input = <<-"END_INPUT"
+            grammar v1
+            config default
+              append VARIABLE=#{character}
+            end
+          END_INPUT
+
+          fig %w<--publish foo/1.2.3>, input, :fork => false
 
           out, * = check_published_grammar_version(1)
 
