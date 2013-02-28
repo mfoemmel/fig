54f19108935af56cb277969bf119f2c785cb2185
Elliot Shank <eshank@drw.com>
2013-02-28 16:31:42 -0600

    Fix priority of environment variables over ~/.netrc.

--- lib/fig/protocol/netrc_enabled.rb
+++ lib/fig/protocol/netrc_enabled.rb
@@ -24,15 +24,21 @@ module Fig::Protocol::NetRCEnabled
     end
 
     entry = nil
-    begin
-      login_data = Net::Netrc.locate host
-      if login_data
-        entry = NetRCEntry.new login_data.login, login_data.password
-      elsif prompt_if_missing
-        entry = get_authentication_from_user(host)
+    if prompt_if_missing
+      entry = get_authentication_from_environment
+    end
+
+    if ! entry
+      begin
+        login_data = Net::Netrc.locate host
+        if login_data
+          entry = NetRCEntry.new login_data.login, login_data.password
+        elsif prompt_if_missing
+          entry = get_authentication_from_user(host)
+        end
+      rescue SecurityError => error
+        raise Fig::UserInputError.new error.message
       end
-    rescue SecurityError => error
-      raise Fig::UserInputError.new error.message
     end
 
     @netrc_entries_by_host[host] = entry
@@ -40,15 +46,31 @@ module Fig::Protocol::NetRCEnabled
     return entry
   end
 
+  def get_authentication_from_environment()
+    username = ENV['FIG_USERNAME']
+    password = ENV['FIG_PASSWORD']
+
+    if username.nil? && password.nil?
+      return nil
+    end
+
+    if ! username.nil? && ! password.nil?
+      return NetRCEntry.new username, password
+    end
+
+    if password.nil?
+      raise Fig::UserInputError.new \
+        'FIG_USERNAME is set but FIG_PASSWORD is not.'
+    end
+
+    raise Fig::UserInputError.new 'FIG_PASSWORD is set but FIG_USERNAME is not.'
+  end
+
   def get_authentication_from_user(host)
-    username =
-      ENV['FIG_USERNAME'] ||
-      HighLine.new.ask("Username for #{host}: ") { |q| q.echo = true }
-    password =
-      ENV['FIG_PASSWORD'] ||
-      HighLine.new.ask("Password for #{username}@#{host}: ") {
-        |q| q.echo = false
-      }
+    username = HighLine.new.ask("Username for #{host}: ") { |q| q.echo = true }
+    password = HighLine.new.ask("Password for #{username}@#{host}: ") {
+      |q| q.echo = false
+    }
 
     return NetRCEntry.new username, password
   end
